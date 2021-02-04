a644a8c836bfb905b5cca54b027286f6e071294c
Evrett <evrettupshall@gmail.com>
2021-02-04 18:44:19 -0500

    Clean

--- SpriteLib3.0-v2.0/SpriteLib3.0-v2.0/PhysicsPlayground.cpp
+++ SpriteLib3.0-v2.0/SpriteLib3.0-v2.0/PhysicsPlayground.cpp
@@ -6,8 +6,7 @@
 PhysicsPlayground::PhysicsPlayground(std::string name)
 	: Scene(name)
 {
-	//No gravity this is a top down scene
-	m_gravity = b2Vec2(0.f, -98.f);
+	m_gravity = b2Vec2(0.f, 0.f);
 	m_physicsWorld->SetGravity(m_gravity);
 
 	m_physicsWorld->SetContactListener(&listener);
@@ -94,42 +93,6 @@ void PhysicsPlayground::InitScene(float windowWidth, float windowHeight)
 		tempPhsBody.SetGravityScale(1.f);
 	}
 
-	{
-		auto entity = ECS::CreateEntity();
-
-		//Add components
-		ECS::AttachComponent<Sprite>(entity);
-		ECS::AttachComponent<Transform>(entity);
-		ECS::AttachComponent<PhysicsBody>(entity);
-
-		//Sets up the components
-		std::string fileName = "hexagon.png";
-		ECS::GetComponent<Sprite>(entity).LoadSprite(fileName, 20, 20);
-		ECS::GetComponent<Sprite>(entity).SetTransparency(1.f);
-		ECS::GetComponent<Transform>(entity).SetPosition(vec3(45.f, -8.f, 3.f));
-
-		auto& tempSpr = ECS::GetComponent<Sprite>(entity);
-		auto& tempPhsBody = ECS::GetComponent<PhysicsBody>(entity);
-
-		float shrinkX = 0.f;
-		float shrinkY = 0.f;
-
-		b2Body* tempBody;
-		b2BodyDef tempDef;
-		tempDef.type = b2_dynamicBody;
-		tempDef.position.Set(float32(0.f), float32(260.f));
-
-		tempBody = m_physicsWorld->CreateBody(&tempDef);
-
-		std::vector<b2Vec2> points = { b2Vec2(-tempSpr.GetWidth() / 2.f, 0), b2Vec2(-tempSpr.GetWidth() / 4.f, -tempSpr.GetHeight() / 2.f), 
-										b2Vec2(tempSpr.GetWidth() / 4.f, -tempSpr.GetHeight() / 2.f), b2Vec2(tempSpr.GetWidth() / 2.f, 0), 
-										b2Vec2(tempSpr.GetWidth() / 4.f, tempSpr.GetHeight() / 2.f),  b2Vec2(-tempSpr.GetWidth() / 4.f, tempSpr.GetHeight() / 2.f) };
-
-		tempPhsBody = PhysicsBody(entity, BodyType::HEXAGON, tempBody, points, vec2(0.f, 0.f), false, HEXAGON, GROUND | OBJECTS | ENVIRONMENT | PLAYER | TRIGGER, 0.5f, 3.5);
-		tempPhsBody.SetColor(vec4(1.f, 0.f, 1.f, 0.3f));
-	}
-	
-
 	ECS::GetComponent<HorizontalScroll>(MainEntities::MainCamera()).SetFocus(&ECS::GetComponent<Transform>(MainEntities::MainPlayer()));
 	ECS::GetComponent<VerticalScroll>(MainEntities::MainCamera()).SetFocus(&ECS::GetComponent<Transform>(MainEntities::MainPlayer()));
 }
@@ -141,206 +104,22 @@ void PhysicsPlayground::Update()
 
 void PhysicsPlayground::GUI()
 {
-	GUIWindowUI();
 
-	if (m_firstWindow)
-	{
-		GUIWindowOne();
-	}
-
-	if (m_secondWindow)
-	{
-		GUIWindowTwo();
-	}
 }
 
 void PhysicsPlayground::GUIWindowUI()
 {
-	ImGui::Begin("Test");
 
-	ImGui::Checkbox("Enable First Window", &m_firstWindow);
-	ImGui::SameLine();
-	if (ImGui::Button("Toggle Second Window", ImVec2(180.f, 15.f)))
-	{
-		m_secondWindow = !m_secondWindow;
-	}
-	
-	ImGui::End();
 }
 
 void PhysicsPlayground::GUIWindowOne()
 {
-	//Window begin
-	ImGui::Begin("Side Docked Window");
-	//is the buffer initialized
-	static bool init = false;
 
-	//Buffers
-	const unsigned int BUF_LEN = 512;
-	static char buf[BUF_LEN];
-	//Image load
-	static std::string imageLoad = "LinkStandby.png";
-	//World gravity slider
-	float gravity[2] = { m_physicsWorld->GetGravity().x, m_physicsWorld->GetGravity().y };
-
-	if (!init)
-	{
-		memset(buf, 0, BUF_LEN);
-		init = true;
-	}
-	m_physicsWorld->SetAllowSleeping(true);
-
-	//ImGui content
-	if (ImGui::TreeNode("Vignette Effect"))
-	{
-		if (EffectManager::GetVignetteHandle() != -1)
-		{
-			if (ImGui::TreeNode("Properties"))
-			{
-				VignetteEffect* vig = (VignetteEffect*)EffectManager::GetEffect(EffectManager::GetVignetteHandle());
-				float innerRad = vig->GetInnerRadius();
-				float outerRad = vig->GetOuterRadius();
-				float opacity = vig->GetOpacity();
-
-				if (ImGui::SliderFloat("Inner Radius", &innerRad, 0.f, outerRad))
-				{
-					vig->SetInnerRadius(innerRad);
-				}
-				if (ImGui::SliderFloat("Outer Radius", &outerRad, innerRad, 1.f))
-				{
-					vig->SetOuterRadius(outerRad);
-				}
-				if (ImGui::SliderFloat("Opacity", &opacity, 0.f, 1.f))
-				{
-					vig->SetOpacity(opacity);
-				}
-
-				ImGui::TreePop();
-			}
-
-			if (ImGui::Button("Remove Effect", ImVec2(100.f, 30.f)))
-			{
-				EffectManager::RemoveEffect(EffectManager::GetVignetteHandle());
-			}
-		}
-		else
-		{
-			if (ImGui::Button("Apply Effect", ImVec2(100.f, 30.f)))
-			{
-				EffectManager::CreateEffect(Vignette, BackEnd::GetWindowWidth(), BackEnd::GetWindowHeight());
-			}
-		}
-
-
-		ImGui::TreePop();
-	}
-
-	if (ImGui::TreeNode("Sepia Effect"))
-	{
-		if (EffectManager::GetSepiaHandle() != -1)
-		{
-			if (ImGui::TreeNode("Properties"))
-			{
-				SepiaEffect* sep = (SepiaEffect*)EffectManager::GetEffect(EffectManager::GetSepiaHandle());
-				float intensity = sep->GetIntensity();
-
-				if (ImGui::SliderFloat("Inner Radius", &intensity, 0.f, 1.f))
-				{
-					sep->SetIntensity(intensity);
-				}
-
-				ImGui::TreePop();
-			}
-
-			if (ImGui::Button("Remove Effect", ImVec2(100.f, 30.f)))
-			{
-				EffectManager::RemoveEffect(EffectManager::GetSepiaHandle());
-			}
-		}
-		else
-		{
-			if (ImGui::Button("Apply Effect", ImVec2(100.f, 30.f)))
-			{
-				EffectManager::CreateEffect(Sepia, BackEnd::GetWindowWidth(), BackEnd::GetWindowHeight());
-			}
-		}
-
-
-		ImGui::TreePop();
-	}
-
-	ImGui::Separator();
-	
-	ImGui::Text("Editing World Variables!");
-	if (ImGui::SliderFloat2("World Gravity", gravity, -1000.f, 1000.f, "%.2f"))
-	{
-		m_physicsWorld->SetGravity(b2Vec2(gravity[0], gravity[1]));
-		m_physicsWorld->SetAllowSleeping(false);
-	}
-
-	ImGui::Separator();
-
-	ImGui::Text("Displaying image dynamically");
-	ImGui::InputText("Input filename for image", buf, BUF_LEN, ImGuiInputTextFlags_CharsNoBlank);
-
-	if (ImGui::Button("Confirm File", ImVec2(100.f, 30.f)))
-	{
-		imageLoad = buf;
-	}
-
-	ImGui::Image((void*)(intptr_t)TextureManager::FindTexture(imageLoad)->GetID(), ImVec2(150.f, 150.f), ImVec2(0, 1), ImVec2(1, 0));
-
-	ImGui::Separator();
-	
-	ImGui::Text("Editing Colors");
-	ImGui::ColorPicker4("Scene Background Color", &m_clearColor.x);
-
-	ImGui::End();
 }
 
 void PhysicsPlayground::GUIWindowTwo()
 {
-	//Second docked window
-	ImGui::Begin("Second Window");
-	//Image load
-	static std::string imageLoad1 = "FunnyButton.png";
-	static std::string imageLoad2 = "LikeButton.png";
-	static std::string hahaPressed = "";
-	static std::string likePressed = "";
-
-	ImGui::BeginTabBar("Tab Bar Test");
-
-	if (ImGui::BeginTabItem("Tab 1"))
-	{
-		ImGui::Text("You are within Tab 1");
-
-		if (ImGui::ImageButton((void*)(intptr_t)TextureManager::FindTexture(imageLoad1)->GetID(), ImVec2(100.f, 100.f), ImVec2(0,1), ImVec2(1,0)))
-		{
-			hahaPressed = "You shouldn't have pressed that...";
-		}
-
-		ImGui::Text("%s", hahaPressed.c_str());
-
-		ImGui::EndTabItem();
-	}
-
-	if (ImGui::BeginTabItem("Tab 2"))
-	{
-		ImGui::Text("You are within Tab 2");
-
-		if (ImGui::ImageButton((void*)(intptr_t)TextureManager::FindTexture(imageLoad2)->GetID(), ImVec2(100.f, 100.f), ImVec2(0, 1), ImVec2(1, 0)))
-		{
-			likePressed = "LIKED!!!";
-		}
-
-		ImGui::Text("%s", likePressed.c_str());
-
-		ImGui::EndTabItem();
-	}
 	
-	ImGui::EndTabBar();
-
-	ImGui::End();
 }
 
 
